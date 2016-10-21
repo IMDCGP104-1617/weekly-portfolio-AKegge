e9031020d060ca95d547d1df7419dff6bb171ffd
AKegge <S186229@uos.ac.uk>
2016-10-21 15:55:16 +0100

    Add files via upload

---
+++ List/List/LinkedList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class LinkedList
+    {
+        public class Node
+        {
+            public object Content;
+            public Node Next;
+        }
+        private Node head;
+        public LinkedList()
+        {
+            head = null;
+        }
+        public void InsertBeginning(object content)
+        {
+            Node newNode = new Node();
+            newNode.Content = content;
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+        }
+        public void InsertAfter(Node after, object content)
+        {
+            Node current = head;
+            while (current != after)
+            {
+                current = current.Next;
+                if (current == null)
+                {
+                    return;
+                }
+            }
+            Node newNode = new Node();
+            newNode.Content = content;
+            current.Next = newNode;
+        }
+        public void RemoveBeginning()
+        {
+            head = null;
+        }
+        public void RemoveAfter(Node after)
+        {
+            Node current = head;
+            while (current != after)
+            {
+                current = current.Next;
+                if (current == null)
+                {
+                    return;
+                }
+            }
+            current = null;
+        }
+        public int Length()
+        {
+            int count = 0;
+            Node current = head;
+            while (current != null)
+            {
+                count++;
+                current = current.Next;
+            }
+            return count;
+        }
+    }
+}
