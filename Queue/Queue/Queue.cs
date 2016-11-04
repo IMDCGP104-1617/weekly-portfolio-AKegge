2f53a8309b4052431a983a53fa69ba1bf3a984af
AKegge <S186229@uos.ac.uk>
2016-11-04 11:12:50 +0000

    Add files via upload

---
+++ Queue/Queue/Queue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Queue
+    {
+        int size = 0;
+        public class Node
+        {
+            public object Content;
+            public Node next;
+        }
+        private Node start;
+        public void CreateQueue()
+        {
+            start = null;
+        }
+        public bool IsEmpty()
+        {
+            return start == null;
+        }
+        public void Enqueue(object content)
+        {
+            Node current = start;
+            while (current != null)
+            {
+                current = current.next;
+            }
+            Node newNode = new Node();
+            newNode.Content = content;
+            current.next = newNode;
+            size++;
+        }
+        public object Dequeue()
+        {
+            if (IsEmpty())
+            {
+                return null;
+            }
+            object dequeued = start.Content;
+            start = start.next;
+            size--;
+            return dequeued;
+        }
+        public void DestroyQueue()
+        {
+            while (start != null)
+            {
+                start = start.next;
+            }
+            size = 0;
+        }
+        public int Length()
+        {
+            return size;
+        }
+        public object Peek()
+        {
+            return start.Content;
+        }
+    }
+}
---
+++ Queue/Queue/QueueTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[TestFixture]
+class QueueTest { }
+[Test]
+public void CreateQueue()
+{
+    
+}
+[Test]
+public void IsEmpty()
+{
+    
+}
+[Test]
+public void Enqueue()
+{
+    
+}
+[Test]
+public void Dequeue()
+{
+
+}
+[Test]
+public void DestroyQueue()
+{
+
+}
+[Test]
+public void Length()
+{
+
+}
+[Test]
+public void Peek()
+{
+
+}
